d70df5d436ad232d7ba44476a5c96dc8670f4673
fajarkarim <fajarabdulkarim@gmail.com>
2017-09-06 22:02:30 +0700

    added get one and learning tensorflow test

--- duperga-server/test/test_get_wishlist.js
+++ duperga-server/test/test_get_wishlist.js
@@ -3,16 +3,16 @@ var chai = require('chai')
 var axios = require('./axios_config')
 var should = chai.should()
 
-var userLogin = {
-  email: 'fajar@gmail.com',
-  password: 'fajar'
-}
 
 describe('GET /wishlist', () => {
 
   var wishlistID
   var token
   var userID
+  var userLogin = {
+    email: 'fajar@gmail.com',
+    password: 'fajar'
+  }
 
   var tokenHeader = {
     headers: {
@@ -138,5 +138,100 @@ describe('GET /wishlist', () => {
 })
 
 describe('GET /wishlist/:userID', () => {
+  var wishlistID
+  var token
+  var userID
+  var userLogin = {
+    email: 'fajar@gmail.com',
+    password: 'fajar'
+  }
 
+  var tokenHeader = {
+    headers: {
+      token: token
+    }
+  }
+
+  var tensor = {
+    data: data
+  }
+
+  before(done => {
+    axios.post('/register', {
+      email: `testing@gmail.com`,
+      password: `testing`
+    })
+    .then(resp => {
+      userID = resp.data._id
+      done()
+    })
+    .catch(err => {
+      err.should.not.exist
+      done()
+    })
+  })
+
+  before(done => {
+    axios.post('/login', {
+      email: `testing@gmail.com`,
+      password: `testing`
+    })
+    .then(resp => {
+      token = resp.data.token
+      done()
+    })
+    .catch(err => {
+      err.should.not.exist
+      done()
+    })
+  })
+
+  after(done => {
+    axios.delete(`/users/${userID}`)
+    .then(resp => {
+      console.log(`deleted`)
+      done()
+    })
+    .catch(err => {
+      console.log('error')
+      done()
+    })
+  })
+
+  it(`Should be an object and have _id property`, done => {
+    axios.get(`/wishlist/${wishlistID}`, tensor, tokenHeader)
+    .then(resp => {
+      resp.data.should.have.property('_id')
+      resp.data.should.be.an('object')
+      done()
+    })
+    .catch(err => {
+      err.should.not.exist
+      done()
+    })
+  })
+
+  it(`Should not an array`, done => {
+    axios.get(`/wishlist/${wishlistID}`, tensor, tokenHeader)
+    .then(resp => {
+      resp.data.should.not.be.an('array')
+      done()
+    })
+    .catch(err => {
+      err.should.not.exist
+      done()
+    })
+  })
+
+  it(`Should not an array`, done => {
+    axios.get(`/wishlist/${wishlistID}`, tensor, tokenHeader)
+    .then(resp => {
+      resp.data.should.not.be.an('array')
+      done()
+    })
+    .catch(err => {
+      err.should.not.exist
+      done()
+    })
+  })
 })
