0e0e76698141cdb6e5bc49b69f00ec898133f233
fajarkarim <fajarabdulkarim@gmail.com>
2017-09-06 17:22:16 +0700

    added create wishlist

--- duperga-server/test/test_create_wishlist.js
+++ duperga-server/test/test_create_wishlist.js
@@ -3,21 +3,61 @@ var chai = require('chai')
 var axios = require('./axios_config')
 var should = chai.should()
 
-var userLogin = {
-  email: 'fajar@gmail.com',
-  password: 'fajar'
+var wishlistData = {
+  name: 'Toyota Camry',
+  budget: 100000000,
+  budget_planning: '5000000 per month',
+  current_price: 300000000,
+  predicted_price: 250000000,
+  userID: 1
 }
 
 describe('POST /wishlist', () => {
 
-  it(`Response should a token`, (done) => {
-    axios.post(`/users/login`, userLogin)
-    .then(({ data }) => {
-      data.should.have.property('token')
+  it(`response should be an object`, (done) => {
+    axios.post(`/wishlist`, wishlistData)
+    .then((resp) => {
+      resp.data.should.be.an('object')
       done()
     })
     .catch(err => {
       err.response.data.status.should.not.equal(404)
+      done()
+    })
+  })
+
+  it(`response should have property _id`, (done) => {
+    axios.post(`/wishlist`, wishlistData)
+    .then((resp) => {
+      resp.data.should.have.property('_id')
+      done()
+    })
+    .catch(err => {
+      err.response.data.status.should.not.equal(404)
+      done()
+    })
+  })
+
+  it(`response should have status 404`, (done) => {
+    axios.post(`/wishlistss`, wishlistData)
+    .then((resp) => {
+      resp.data.should.have.property('_id')
+      done()
+    })
+    .catch(err => {
+      err.response.data.status.should.equal(404)
+      done()
+    })
+  })
+
+  it(`response should not an array`, (done) => {
+    axios.post(`/wishlistss`, wishlistData)
+    .then((resp) => {
+      resp.data.should.not.an('array')
+      done()
+    })
+    .catch(err => {
+      err.response.data.status.should.equal(404)
       done()
     })
   })
